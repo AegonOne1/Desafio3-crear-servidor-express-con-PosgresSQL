762d8933de5fe9f29cd497e086b1723deeb55e48
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-06-16 00:01:10 -0400

    se busca error en basede datos, problema en clave de posgresSQL

--- database/config.js
+++ database/config.js
@@ -1,13 +1,24 @@
-import pg from "pg";
+import  pg  from "pg";
 import "dotenv/config";
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE} = process.env;
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
 
-const pool = new pg.pool({
+
+// Comprobacion de error al conectar con la base de datos problemas en clave
+// console.log("Database connection details:", {
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     database: DB_DATABASE,
+//     port: DB_PORT,
+// });
+
+const pool = new pg.Pool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
+    port: DB_PORT,
     allowExitOnIdle: true
 });
 
@@ -15,7 +26,7 @@ pool.query("SELECT NOW()", (err, res) =>{
     if (err){
         console.log("Error al conectar con DB", err);
     } else {
-        console.log("DB ready to work", res.row[0].now);
+        console.log("DB ready to work", res.rows[0].now);
     };
 });
 
